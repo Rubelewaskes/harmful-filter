d0f8232b9ddc9d30cc77055ce5bfa131880110ac
Grigory <sogrian@yandex.ru>
2025-09-20 19:16:06 +0300

    changed settings

--- llama_cpp/settings.py
+++ llama_cpp/settings.py
@@ -6,7 +6,7 @@ class AppConfig(BaseModel):
     
     API_URL: str = Field("http://127.0.0.1:8000", env="API_URL")
     API_TOKEN: str = Field("very-secret-token", env="API_TOKEN")
-    MODEL_PATH: str = Field("Q4_K_M.gguf", env="MODEL_PATH")
+    MODEL_PATH: str = Field("Q8_0.gguf", env="MODEL_PATH")
     FILE_PATH: str = Field("chats-aia/chats-aia/chats/", env="FILE_PATH")
     OUT_FILE: str = Field("results.json", env="OUT_FILE")
 
