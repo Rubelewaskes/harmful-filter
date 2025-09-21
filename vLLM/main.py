c0f907779fcd11b31c9eeaa9a4f1cee334f69980
Grigory <sogrian@yandex.ru>
2025-09-21 09:24:38 +0300

    changet to main

--- vLLM/main.py
+++ vLLM/main.py
@@ -122,4 +122,4 @@ async def process_chats(payload: List[ChatPayload]):
     return {"results": results}
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8001, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
